38967c7184bd8e4271cf02ccee1f7fe777dd9241
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-28 18:26:11 -0300

    initial commit

---
+++ src/main/java/com/vinisnzy/order_reader/OrderReaderApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.order_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/order_reader/OrderReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.order_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
